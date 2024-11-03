7966ee8be8ad95b27b5e7deddea11d850a050b0f
el-arma <>
2024-11-04 00:39:13 +0100

    Adding new classes and refactorin
    Piece type class added - name of the pieces.
    Placement of pieces refactored.
    Class Players added.
    Few guidelines in comment added.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # CHESS ENGINE
-# v.1.0.0
+# v.1.0.1
+
 
 from enum import Enum
 import numpy as np
@@ -7,9 +8,9 @@ import numpy as np
 # Set the print options in numpy so that the chess boards print nicely: 
 np.set_printoptions(edgeitems = 30, linewidth = 250, formatter = dict(float = lambda x: "%.3g" % x))
 
-class Pieces_Types(Enum):
+class Piece_Icon(Enum):
     
-    # black pieces symbols:
+    # white pieces symbols:
     WHITE_KING = '♔'
     WHITE_QUEEN = '♕'
     WHITE_TOWER = '♖'
@@ -24,21 +25,33 @@ class Pieces_Types(Enum):
     BLACK_BISHOP = '♝'
     BLACK_KNIGHT = '♞'
     BLACK_PAWN = '♟'
-    
-class Pieces_Col(Enum):
+
+class Piece_Type(Enum):
+    KING = 'king'
+    QUEEN = 'queen'
+    TOWER = 'tower'
+    BISHOP = 'bishop'
+    KNIGHT = 'knight'
+    PAWN = 'pawn'
+
+class Piece_Col(Enum):
     BLACK = 'B'
     WHITE = 'W'
 
-class Tiles(Enum):
+class Tile(Enum):
     WHITE = ' □ '
     BLACK = ' ■ '
 
-class Alt_Colors(Enum):
+class Alt_Color(Enum):
+
     GREEN = '\033[92m'
-    ENDC = '\033[0m'
-
-
-
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    COLOR_OFF = '\033[0m'
 
 ################################################################################################
 # TILES:
@@ -50,46 +63,57 @@ class Board_tile:
         self.tile_white = tile_white
         
         if self.tile_white:
-            self.symbol = Tiles.WHITE.value
+            self.symbol = Tile.WHITE.value
         else:
-            self.symbol = Tiles.BLACK.value
+            self.symbol = Tile.BLACK.value
 
     def __repr__(self) -> str:
         return self.symbol
 
 ################################################################################################
-# GAME PIECES:
+# CHESS PIECES:
+# probably to be moved to a separate file
 
 class Piece:
-    def __init__(self, color: Pieces_Col, name,
-                  piece_type: Pieces_Types) -> None:
+    def __init__(self, color: Piece_Col, piece_type: Piece_Type, icon: Piece_Icon) -> None:
         
         self.color = color
-        self.name = name
         self.piece_type = piece_type
+        self.icon = icon
     
-    def move(self, field):
+    def move(self, field: str) -> None:
+        # something like this:
+        # 7a>6a (move fig. from field 7a to 6a) we could use ">" operator overload 
+        # alternatively or perhapes also use simplyfiy chess nontation:
+        # see: https://www.chess.com/terms/chess-notation
+
         pass
 
     def possible_moves(self):
         pass
 
     def __repr__(self) -> str:
+        # return garphical representation of a piece
         
-        if self.color == Pieces_Col.BLACK.value:
+        if self.color == Piece_Col.BLACK.value:
             
-            prnt_me = f'{Alt_Colors.GREEN.value} {self.piece_type} {Alt_Colors.ENDC.value}'
+            prnt_me = f'{Alt_Color.GREEN.value} {self.icon} {Alt_Color.COLOR_OFF.value}'
+            # for better viability 'black' are green
         
         else:
             
-            prnt_me = f' {self.piece_type} '
-        
+            prnt_me = f' {self.icon} '
+         
         return prnt_me
 
+# remember about super() method:
 class Pawn(Piece):
     pass
 
 class Knight(Piece):
+    pass
+
+class Bishop(Piece):
     pass
 
 class Tower(Piece):
@@ -105,63 +129,125 @@ class King(Queen):
 ################################################################################################
 # GAME:
 
+class Player:
+
+    # instance counter:
+    inst_cnt = 0
+
+    def __init__(self, player_name: str = None) -> None:
+        # much, much later at the end ask player about the name (input())
+
+        self.player_name = player_name
+
+        Player.inst_cnt += 1
+        
+        if Player.inst_cnt > 2:
+            raise Exception("You can't have more then two Players!")
+
+        if self.player_name is None:
+            # if a player dont want a name or during unit tests
+
+            self.player_name = f'Player_{Player.inst_cnt}'
+
+        else:
+            self.player_name = player_name
+
+        return None
+
+class Spectator:
+    # perhapse?
+    # definetly not a priority
+    pass
+
 class Game:
 
     def __init__(self) -> None:
         
+        self.p1: Player = Player()
+        self.p2: Player = Player()
+
         # CREATE EMPTY CHESS BOARD:
-        white_tile = Board_tile(True)
-        black_tile = Board_tile(False)
+        self.white_tile = Board_tile(True)
+        self.black_tile = Board_tile(False)
 
-        self.board = np.full((8, 8), white_tile, dtype = 'O')
+        # start with all tiles white
+        self.board = np.full((8, 8), self.white_tile, dtype = 'O')
 
-        self.board[::2, 1::2] = black_tile  
+        # add black tiles p.1
+        self.board[::2, 1::2] = self.black_tile  
         # Setting alternate rows starting from the first row with alternate columns to 
 
-        self.board[1::2, ::2] = black_tile
+        # add black tiles p.2
+        self.board[1::2, ::2] = self.black_tile
         # Setting alternate rows starting from the second row with alternate columns to 1
 
-        # PLACE PIECES:
-        # (REFACTORISATION REQUIRED!)
-        self.board[1,:] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_PAWN.value)
-        self.board[6,:] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_PAWN.value)
+        # PLACE PIECES ON THE BOARD:
+        # (require furhter refactorization)
 
-        self.board[0,7] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_TOWER.value)
-        self.board[0,0] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_TOWER.value)
+        self.white_pawn = Pawn(color = Piece_Col.WHITE.value, piece_type = Piece_Type.PAWN.value, 
+                               icon = Piece_Icon.WHITE_PAWN.value)
+        self.black_pawn = Pawn(color = Piece_Col.BLACK.value, piece_type = Piece_Type.PAWN.value, 
+                               icon = Piece_Icon.BLACK_PAWN.value)
+        
+        self.white_tower =  Tower(color = Piece_Col.WHITE.value, piece_type = Piece_Type.TOWER.value, 
+                                  icon = Piece_Icon.WHITE_TOWER.value)
+        self.black_tower =  Tower(color = Piece_Col.BLACK.value, piece_type = Piece_Type.TOWER.value, 
+                                  icon = Piece_Icon.BLACK_TOWER.value)
+        
+        self.white_knight = Knight(color = Piece_Col.WHITE.value, piece_type = Piece_Type.KNIGHT.value, 
+                                   icon = Piece_Icon.WHITE_KNIGHT.value)
+        self.black_knight = Knight(color = Piece_Col.BLACK.value, piece_type = Piece_Type.KNIGHT.value, 
+                                   icon = Piece_Icon.BLACK_KNIGHT.value)
 
-        self.board[7,7] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_TOWER.value)
-        self.board[7,0] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_TOWER.value)
+        self.white_bishop = Bishop(color = Piece_Col.WHITE.value, piece_type = Piece_Type.BISHOP.value, 
+                                   icon = Piece_Icon.WHITE_BISHOP.value)
+        self.black_bishop = Bishop(color = Piece_Col.BLACK.value, piece_type = Piece_Type.BISHOP.value, 
+                                   icon = Piece_Icon.BLACK_BISHOP.value)
 
+        self.white_queen = Queen(color = Piece_Col.WHITE.value, piece_type = Piece_Type.QUEEN.value, 
+                                 icon = Piece_Icon.WHITE_QUEEN.value)
+        self.black_queen = Queen(color = Piece_Col.BLACK.value, piece_type = Piece_Type.QUEEN.value, 
+                                 icon = Piece_Icon.BLACK_QUEEN.value)
 
-        self.board[0,6] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_KNIGHT.value)
-        self.board[0,1] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_KNIGHT.value)
+        self.white_king = King(color = Piece_Col.WHITE.value, piece_type = Piece_Type.KING.value, 
+                               icon = Piece_Icon.WHITE_KING.value)
+        self.black_king = King(color = Piece_Col.BLACK.value, piece_type = Piece_Type.KING.value, 
+                               icon = Piece_Icon.BLACK_KING.value)
 
-        self.board[7,6] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_KING.value)
-        self.board[7,1] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_KING.value)
+        # place pawns:
+        self.board[1,:] = self.black_pawn
+        self.board[6,:] = self.white_pawn
+        
+        # place towers:
+        self.board[0,7] = self.black_tower
+        self.board[0,0] = self.black_tower
+        self.board[7,0] = self.white_tower
+        self.board[7,7] = self.white_tower
 
-        self.board[0,1] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_KNIGHT.value)
-        self.board[0,6] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_KNIGHT.value)
+        # place knights:
+        self.board[0,1] = self.black_knight
+        self.board[0,6] = self.black_knight
+        self.board[7,1] = self.white_knight
+        self.board[7,6] = self.white_knight
+        
+        # place bishops:
+        self.board[0,2] = self.black_bishop
+        self.board[0,5] = self.black_bishop
+        self.board[7,2] = self.white_bishop
+        self.board[7,5] = self.white_bishop
 
-        self.board[7,1] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_KING.value)
-        self.board[7,6] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_KING.value)
-
-
-        self.board[0,2] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_BISHOP.value)
-        self.board[0,5] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_BISHOP.value)
-
-        self.board[7,2] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_BISHOP.value)
-        self.board[7,5] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_BISHOP.value)
-
-
-        self.board[0,3] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_QUEEN.value)
-        self.board[0,4] = Piece(color = Pieces_Col.BLACK.value, name = 'piece', piece_type = Pieces_Types.BLACK_KING.value)
-
-        self.board[7,3] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_QUEEN.value)
-        self.board[7,4] = Piece(color = Pieces_Col.WHITE.value, name = 'piece', piece_type = Pieces_Types.WHITE_KING.value)
+        # place queens:
+        self.board[7,3] = self.white_queen
+        self.board[0,3] = self.black_queen
+        
+        # place kings:
+        self.board[7,4] = self.white_king
+        self.board[0,4] = self.black_king
+        
 
     def display_board(self) -> None:
 
-        print()
+        print('\n' + ' ' * 6 + '-' * 30)
 
         for idx, row in enumerate(self.board):
             print(8 - idx, ' ' , row)
@@ -180,5 +266,4 @@ if __name__ == "__main__":
     g1 = Game()
 
     g1.display_board()
-
 
